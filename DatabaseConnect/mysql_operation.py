1ea9c02ae43130c1320a71c78dea409756ab34cc
PC-201712281723\Administrator <dengmingyao2015@outlook.com>
2018-06-02 21:41:26 +0800

    数据库操作优化

--- DatabaseConnect/mysql_operation.py
+++ DatabaseConnect/mysql_operation.py
@@ -18,12 +18,27 @@ import pymysql
 import yaml
 import io
 import logging
-import sys
+from functools import wraps
+
+
+def singleton(cls):
+    instances = {}
+
+    @wraps(cls)
+    def get_instance(*args, **kw):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kw)
+        return instances[cls]
+    return get_instance
 
 
 # 获取数据库连接，配置数据库IP，端口等信息，获取数据库连接
+@singleton
 class MySQLOperation:
-    def __init__(self, config_file, db_name):
+    def __init__(self):
+        self.connection = None
+
+    def get_connection(self, config_file, db_name):
         with io.open(config_file, 'r', encoding='utf-8') as stream:
             yaml_content = yaml.load(stream)
 
@@ -35,46 +50,66 @@ class MySQLOperation:
             charset = yaml_content[db_name]['charset']
 
         try:
-            self.dbconn = pymysql.connect(host=host, port=port, user=user, password=password, database=db_name,
+            self.connection = pymysql.connect(host=host, port=port, user=user, password=password, database=db_name,
                                           charset=charset)
         except Exception as e:
             logging.error('初始化数据连接失败：%s' % e)
-            sys.exit()
+
+        return self.connection
 
     # 返回数据库的一条记录
     def select_one_record(self, query, data=""):
         logging.info('query：%s  data：%s' % (query, data))
-        db_cursor = self.dbconn.cursor()
+        # db_cursor = self.connection.cursor()
         query_result = None
-        try:
-            if data:
-                db_cursor.execute(query, data)
-            else:
-                db_cursor.execute(query)
-            query_result = db_cursor.fetchone()
-        except Exception as e:
-            logging.error('执行数据库查询操作失败：%s' % e)
-        finally:
-            db_cursor.close()
-            return query_result
-
-    def execute_insert(self, query, data=''):
-        logging.info('query：%s  data：%s' % (query, data))
-        with self.dbconn as cursor:
+        # try:
+        #     if data:
+        #         db_cursor.execute(query, data)
+        #     else:
+        #         db_cursor.execute(query)
+        #     query_result = db_cursor.fetchone()
+        # except Exception as e:
+        #     logging.error('执行数据库查询操作失败：%s' % e)
+        # finally:
+        #     db_cursor.close()
+        # return query_result
+        with self.connection as cursor:
             if data:
                 cursor.execute(query, data)
             else:
                 cursor.execute(query)
-        cursor.close()
+            query_result = cursor.fetchone()
+        return query_result
 
-    def close_dbconn(self):
-        self.dbconn.close()
+    def execute_insert(self, query, data=''):
+        logging.info('query：%s  data：%s' % (query, data))
+        with self.connection as cursor:
+            if data:
+                cursor.execute(query, data)
+            else:
+                cursor.execute(query)
+
+    def close_connection(self):
+        self.connection.close()
 
 
 if __name__ == '__main__':
-    mysql_connect = MySQLOperation('dbconfig.yml', 'caibaotc')
-    query = "SELECT * FROM caibaotc.tc_order order by gmt_create DESC limit 10"
-    result = mysql_connect.select_one_record(query)
-    mysql_connect.close_dbconn()
-    print(result)
+    mysql_single1 = MySQLOperation()
+    connection1 = mysql_single1.get_connection('dbconfig.yml', 'caibaotc')
+    query1 = "SELECT * FROM caibaotc.tc_order where viewer_id1 = 'A00300100000035' and local_order_no = 'TCAP1704270948504427757832'"
+    result1 = mysql_single1.select_one_record(query1)
+    mysql_single1.close_connection()
+
+    print(result1)
+
+    mysql_single2 = MySQLOperation()
+    connection2 = mysql_single2.get_connection('dbconfig.yml', 'zypPlatformTest')
+    query2 = "SELECT * FROM zypPlatformTest.zyp_merchant where user_code = 'A00300100000035'"
+    result2 = mysql_single2.select_one_record(query2)
+    mysql_single2.close_connection()
+    print(result2)
+
+
+
+
 
